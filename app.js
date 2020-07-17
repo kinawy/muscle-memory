35ad35415cbf31e44b4fbe2a58228511657745b8
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-17 19:09:35 -0400

    [appending divs etc] added new divs paragraphs and sections

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ sectionOne.textContent = 'This is Section One';
 sectionTwo.textContent = 'This is Section Two';
 sectionThree.textContent = 'This is Section Three';
 sectionFour.textContent = 'This is Section Four';
-sectionFive.textContent = 'This is Sectiono Five';
+sectionFive.textContent = 'This is Section Five';
 
 const paraOne = document.querySelector('.paragraph1');
 paraOne.textContent = 'Sameh Kinawy';
@@ -81,6 +81,40 @@ const paraFour = document.querySelector('.paragraph4');
 paraFour.textContent = 'Jaylen Brown';
 const paraFive = document.querySelector('.paragraph5');
 paraFive.textContent = 'Gordon Hayward';
+
+const containerSix = document.createElement('div');
+containerSix.classList.add('container6');
+const sectionSix = document.createElement('h2');
+
+sectionSix.setAttribute('id','section6');
+sectionSix.textContent = 'Celtics';
+
+const paraSix = document.createElement('p');
+paraSix.classList.add('paragraph6');
+paraSix.textContent = 'James Harden';
+
+const body = document.querySelector('body');
+body.appendChild(containerSix);
+containerSix.appendChild(sectionSix);
+containerSix.appendChild(paraSix);
+
+const containerSeven = document.createElement('div');
+containerSeven.classList.add('container7');
+const sectionSeven = document.createElement('h2');
+
+sectionSeven.setAttribute('id', 'section7');
+sectionSeven.textContent = 'More Celtics';
+
+const paraSeven = document.createElement('p');
+paraSeven.classList.add('paragraph7');
+paraSeven.textContent = 'Another Basketball Player';
+
+body.appendChild(containerSeven);
+containerSeven.appendChild(sectionSeven);
+containerSeven.appendChild(paraSeven);
+
+
+
 
 
 

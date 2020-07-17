3a72aa8dee75532fc8085190d61c9c4b044263be
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-17 17:36:22 -0400

    Blind Code Along

--- app.js
+++ app.js
@@ -7,5 +7,55 @@ let mySelf = {
     age: 30,
     hobbies: ['gaming', 'coding', 'bbqing'],
     standing: true
+};
+console.log(str, num, boo, favTeams, mySelf);
+
+let arrFunc = function(array) {
+    for (let i =0; i<= array.length; i++) {
+        let how = array[i]);
+    }
 }
-console.log(str, num, boo, favTeams, mySelf)
+
+
+console.log(arrFunc(favTeams));
+
+let callFunc2 = function(type) {
+    if (typeof type === 5) {
+        return type;
+    }
+    
+}
+
+let callFunc = function (callback) {
+    if (callback === true) {
+        console.log('Great Success')
+    }
+}
+
+callFunc(callFunc2(num))
+
+let strFunc = function(string) {
+    if (string === 'string') {
+        return string.split(' ');
+    }
+}
+
+strFunc(str);
+
+let booFunc = function(boolean) {
+    if (boolean == true) {
+        console.log(('Yes, this is true'))
+    }
+}
+
+booFunc(boo);
+
+let ranFun = function(obj) {
+    for (let i = 0; i<=obj.length;i++) {
+        let two = obj[i];
+    }
+}
+
+console.log(ranFun(mySelf));
+
+

70536753e5a780ae9086a277d061ebd72df16819
kinawy <sameh.yasser.kinawy@gmail.com>
2020-07-17 17:07:55 -0400

    [variables] Added 5 variables

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+let str = 'string';
+let num = 5;
+let boo = true;
+let favTeams = ['Red Sox', 'Patriots', 'Bruins', 'Liverpool', 'Celtics'];
+let mySelf = {
+    name: 'Sameh',
+    age: 30,
+    hobbies: ['gaming', 'coding', 'bbqing'],
+    standing: true
+}
+console.log(str, num, boo, favTeams, mySelf)
